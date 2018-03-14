ee73856f7a4afc6dfcd47a568cab9ca6e05e541a
Tamara Hills <thills@mozilla.com>
2018-03-13 18:05:17 -0700

    First commit.

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var db = require('./db');
+var app = express();
+
+// app.js
+app.get('/api', function (req, res) {
+  res.status(200).send('API works.');
+});
+
+app.get('/hello', function(req, res) {
+  res.status(200).send('hello world');
+});
+
+var UserController = require('./user/UserController');
+app.use('/api/users', UserController);
+
+
+var AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+
+var CommandController = require('./command/CommandController');
+app.use('/command', CommandController);
+ 
+module.exports = app;
