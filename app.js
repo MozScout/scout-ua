633d21eb43b6967bc75f42a45b1159cdea160da4
Tamara Hills <thills@mozilla.com>
2018-03-21 17:58:18 -0700

    commit.

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
-// app.js
-app.get('/api', function (req, res) {
-  res.status(200).send('API works.');
-});
-
-app.get('/hello', function(req, res) {
-  res.status(200).send('hello world');
-});
-/*
-var UserController = require('./user/UserController');
-app.use('/api/users', UserController);
-
-
-var AuthController = require('./auth/AuthController');
-app.use('/api/auth', AuthController);
-
-var CommandController = require('./command/CommandController');
-app.use('/command', CommandController);*/
- 
+app.get('/', (req, res) => res.send('Hello World!'))
 module.exports = app;
--- test/chai.js
+++
@@ -1,7 +0,0 @@
-/**
- * Module to group together our chai use and plugins.
- */
-const chai = require('chai');
-
-
-module.exports = chai;
