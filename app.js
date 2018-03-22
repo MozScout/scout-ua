41acd7692c4ba9e7d444e98c33f48c61e2edb1e9
Tamara Hills <thills@mozilla.com>
2018-03-21 18:18:41 -0700

    status.

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.get('/hello', function(req, res) {
   res.status(200).send('hello world');
 });
 
-var UserController = require('./user/UserController');
+/*var UserController = require('./user/UserController');
 app.use('/api/users', UserController);
 
-/*
+
 var AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController);
 
