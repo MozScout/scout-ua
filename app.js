0cfa63fbe11666076a75e2018b4380eb03f50071
Tamara Hills <thills@mozilla.com>
2018-03-21 18:15:24 -0700

    commit.

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.js
+app.get('/api', function (req, res) {
+  res.status(200).send('API works.');
+});
+
+app.get('/hello', function(req, res) {
+  res.status(200).send('hello world');
+});
+
+var UserController = require('./user/UserController');
+app.use('/api/users', UserController);
+
+/*
+var AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+
+var CommandController = require('./command/CommandController');
+app.use('/command', CommandController);*/
+ 
 module.exports = app;
