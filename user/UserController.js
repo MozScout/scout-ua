71ac3d94e0a8dfe97d13086849b5721a3614c531
Dylan Oliver <d@doliver.net>
2018-05-11 12:12:12 -0700

    Add JWT auth on /scoutusers api

--- user/UserController.js
+++ user/UserController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ScoutUser = require('../data/models/ScoutUser');
+const VerifyToken = require('../VerifyToken');
 const Database = require('../data/database');
 const database = new Database();
 
@@ -8,7 +9,7 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-router.get('/scoutusers', async function(req, res) {
+router.get('/scoutusers', VerifyToken, async function(req, res) {
   try {
     const articles = await ScoutUser.scan().exec();
     res.send(articles);
@@ -18,7 +19,7 @@ router.get('/scoutusers', async function(req, res) {
   }
 });
 
-router.post('/scoutusers', async function(req, res) {
+router.post('/scoutusers', VerifyToken, async function(req, res) {
   try {
     console.log(req.body);
     await database.processScoutUser(req.body.userid, req.body.access_token);
