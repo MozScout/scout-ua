0f0b232f2cbceb06d2eff99e684a481c08c2f907
Benoit Zhong <bzhong@mozilla.com>
2018-06-20 10:26:27 -0700

    Removed all camelCase from API

--- command/CommandController.js
+++ command/CommandController.js
@@ -78,20 +78,16 @@ router.post('/intent', VerifyToken, async function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     switch (req.body.cmd) {
       case 'ScoutTitles':
-        scoutTitles(
-          req.body.userid,
-          res,
-          req.body.extendedData == true || req.body.extended_data == true
-        );
+        scoutTitles(req.body.userid, res, req.body.extended_data == true);
         break;
       case 'SearchAndPlayArticle':
       case 'SearchAndSummarizeArticle':
         searchAndPlayArticle(
           res,
           req.body.userid,
-          req.body.searchTerms ? req.body.searchTerms : req.body.search_terms,
+          req.body.search_terms,
           req.body.cmd === 'SearchAndSummarizeArticle',
-          req.body.extendedData == true || req.body.extended_data == true
+          req.body.extended_data == true
         );
         break;
       case 'ScoutMyPocket':
@@ -123,7 +119,7 @@ router.post('/article', VerifyToken, async function(req, res) {
     const result = await processArticleRequest(
       req,
       false,
-      req.body.extendedData == true || req.body.extended_data == true
+      req.body.extended_data == true
     );
     res.status(200).send(JSON.stringify(result));
   } catch (reason) {
@@ -140,7 +136,7 @@ router.post('/summary', VerifyToken, async function(req, res) {
     const result = await processArticleRequest(
       req,
       true,
-      req.body.extendedData == true || req.body.extended_data == true
+      req.body.extended_data == true
     );
     res.status(200).send(JSON.stringify(result));
   } catch (reason) {
@@ -154,7 +150,7 @@ router.get('/search', VerifyToken, async function(req, res) {
   try {
     const titles = await getTitlesFromPocket(
       req.query.userid,
-      req.body.extendedData == true || req.body.extended_data == true
+      req.body.extended_data == true
     );
     const article = await findBestScoringTitle(req.query.q, titles.articles);
     const result = `Search for: ${req.query.q}, identified article: ${
@@ -381,9 +377,7 @@ async function getArticleMetadata(pocketArticle, extendedData) {
     resolved_url: pocketArticle.resolved_url,
     title: pocketArticle.resolved_title,
     author,
-    lengthMinutes,
     length_minutes: lengthMinutes,
-    imageURL: pocketArticle.top_image_url,
     image_url: pocketArticle.top_image_url
   };
 
