538609272cc38b9c913402216d566145edccaa96
Benoit Zhong <bzhong@mozilla.com>
2018-07-20 11:02:11 -0700

    Only use titles with is_article field

--- command/CommandController.js
+++ command/CommandController.js
@@ -304,20 +304,22 @@ async function scoutSummaries(userid, jsonBodyAttr, urlAttr, titleAttr, res) {
           let promiseArray = [];
           let arrJson = jsonBody[jsonBodyAttr];
           Object.keys(arrJson).forEach(key => {
-            summaryOptions.uri = summaryLink + arrJson[key][urlAttr];
-            logger.debug('Summary uri is: ' + summaryOptions.uri);
-            promiseArray.push(
-              rp(summaryOptions)
-                .then(sumResults => {
-                  let sumResultsJson = JSON.parse(sumResults);
-                  sumResultsJson['title'] = arrJson[key][titleAttr];
-                  return sumResultsJson;
-                })
-                .catch(function(err) {
-                  logger.error('Caught an error: ' + err);
-                  return JSON.stringify({});
-                })
-            );
+            if (arrJson[key].is_article == '1') {
+              summaryOptions.uri = summaryLink + arrJson[key][urlAttr];
+              logger.debug('Summary uri is: ' + summaryOptions.uri);
+              promiseArray.push(
+                rp(summaryOptions)
+                  .then(sumResults => {
+                    let sumResultsJson = JSON.parse(sumResults);
+                    sumResultsJson['title'] = arrJson[key][titleAttr];
+                    return sumResultsJson;
+                  })
+                  .catch(function(err) {
+                    logger.error('Caught an error: ' + err);
+                    return JSON.stringify({});
+                  })
+              );
+            }
           });
           logger.debug('RETURNING PROMISE.ALL ' + Date.now());
           return Promise.all(promiseArray);
@@ -388,7 +390,10 @@ async function getTitlesFromPocket(userid, extendedData) {
 
       // process list of articles
       Object.keys(jsonBody.list).forEach(key => {
-        if (jsonBody.list[key].resolved_title) {
+        if (
+          jsonBody.list[key].resolved_title &&
+          jsonBody.list[key].is_article == '1'
+        ) {
           articlesPromises.push(
             getArticleMetadata(jsonBody.list[key], extendedData)
           );
@@ -530,10 +535,23 @@ async function searchForPocketArticle(getBody, searchTerm, extendedData) {
       logger.debug('keysarr = ' + keysArr);
       logger.info('article count: ' + keysArr.length);
       if (keysArr.length > 0) {
-        result = await getArticleMetadata(
-          jsonBody.list[keysArr[0]],
-          extendedData
-        );
+        let isArticleId = 0;
+        while (
+          isArticleId < keysArr.length &&
+          jsonBody.list[keysArr[isArticleId]].is_article != '1'
+        ) {
+          isArticleId++;
+        }
+        if (isArticleId < keysArr.length) {
+          result = await getArticleMetadata(
+            jsonBody.list[keysArr[isArticleId]],
+            extendedData
+          );
+        } else {
+          logger.warn(
+            `Searching for '${searchTerm}' failed to find a matching article.`
+          );
+        }
       }
     } else {
       logger.warn(
@@ -678,10 +696,10 @@ function findBestScoringTitle(searchPhrase, articleMetadataArray) {
       if (iCount >= articleMetadataArray.length) {
         logger.debug('Done getting results.');
         logger.debug('Max Score is: ' + maxValue);
-        logger.info('Article is: ' + articleMetadataArray[curMaxIndex].title);
         if (maxValue === 0) {
           reject('NoMatchFound');
         }
+        logger.info('Article is: ' + articleMetadataArray[curMaxIndex].title);
         resolve(articleMetadataArray[curMaxIndex]);
       }
     });
