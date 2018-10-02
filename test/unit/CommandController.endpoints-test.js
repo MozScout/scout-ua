cfc6b5712f84fe4c3c9e681b5b5c28fca48608d8
Tamara Hills <thills@mozilla.com>
2018-10-01 22:16:11 -0700

    debug

--- test/unit/CommandController.endpoints-test.js
+++ test/unit/CommandController.endpoints-test.js
@@ -6,8 +6,8 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 //const AudioFileHelper = require('../../command/AudioFileHelper');
-const polly_tts = require('../../command/polly_tts');
-const statusHelper = require('../../articlestatus/ArticleStatusHelper.js');
+//const polly_tts = require('../../command/polly_tts');
+//const statusHelper = require('../../articlestatus/ArticleStatusHelper.js');
 const HostnameHelper = require('../../command/HostnameHelper.js');
 
 const expect = chai.expect;
