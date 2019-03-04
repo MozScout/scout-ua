4394d008fb3a6532ebcabb5cda672aaaf3c1d67e
Tamara Hills <thills@mozilla.com>
2019-03-03 20:35:41 -0800

    debyg

--- command/CommandController.js
+++ command/CommandController.js
@@ -323,7 +323,7 @@ router.post('/webpage', VerifyToken, async function(req, res) {
         req.body.locale,
         summ
       );
-      logger.debug('Mobile metadata is: ' + mobileMetadata);
+      logger.debug('Mobile metadata is: ' + JSON.parse(mobileMetadata));
       // Do we have the article cached?
       if (mobileMetadata && mobileMetadata.length > 0) {
         // We have already processed this article
@@ -335,7 +335,7 @@ router.post('/webpage', VerifyToken, async function(req, res) {
           version
         );
         mData.audio_url = response.url;
-        logger.debug('Before response: ' + mData);
+        logger.debug('Before response: ' + JSON.parse(mData));
         res.status(200).send(JSON.stringify(mData));
       } else {
         logger.debug('This article is not cached');
